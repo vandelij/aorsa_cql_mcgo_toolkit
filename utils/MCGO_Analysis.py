4103cf57985dc168de6b52d4ff77c2972a007ad3
JacobvandeLindt <jacobvandelindt@gmail.com>
2025-06-18 15:31:02 -0400

    added ability to convert mcgo particle list to p2f particle list

--- utils/MCGO_Analysis.py
+++ utils/MCGO_Analysis.py
@@ -1,5 +1,5 @@
 ##################################
-#   Classes for working with both setup and post-proccesing of MCGO
+#   Classes for working with both setup and post-proccesing of MCGO with P2F
 #   Author: Jacob van de Lindt
 #   Date: 6/12/2025
 #################################
@@ -508,3 +508,269 @@ class MCGO_Post_Process:
         # Save to a new file
         with open(out_file, "w") as f:
             f.writelines(output_lines)
+    
+    def load_p2f_output(self, p2f_out_file):
+        self.p2f_out = netCDF4.Dataset(p2f_out_file, "r")
+        # load in the 4D distribution and its grid once 
+        self.p2f_f_vpar_vper_z_r = self.p2f_out.variables['f_rzvv'][:]
+        self.p2f_rbin_centers = self.p2f_out.variables['R_binCenters'][:]
+        self.p2f_zbin_centers = self.p2f_out.variables['z_binCenters'][:]
+        self.p2f_vperp_bin_centers = self.p2f_out.variables['vPer_binCenters'][:]
+        self.p2f_vpar_bin_centers = self.p2f_out.variables['vPar_binCenters'][:]
+
+        self.p2f_vmax = np.max(np.union1d(self.p2f_vperp_bin_centers, self.p2f_vpar_bin_centers))
+    
+    def p2f_plot_density_RZ(self, figsize=(5,5), cmap='jet', return_plot=False):
+        density = self.p2f_out.variables['density'][:]
+
+        fig, ax = plt.subplots(figsize=(5,5))
+
+        ax.axis('equal')
+        ax.set_xlabel('R [m]')
+        ax.set_ylabel('Z [m]')
+        ax.set_title('P2F Density')
+
+        # convert from 1/cm^3 to 1/m^3
+        contour = ax.contourf(self.p2f_rbin_centers, self.p2f_zbin_centers, density*1e6, levels=100, cmap=cmap)
+        fig.colorbar(contour, ax=ax, label=r'Density [m$^{-3}$]')
+
+        if return_plot:
+            return fig, ax
+
+        plt.show()     
+
+    def p2f_get_distribution_function_at_RZ(self, indexR, indexZ):
+        """Grabs the f(vpar, vperp) at radial bin center R[indexR], axial bin center Z[indexZ]
+            creates meshgrid of velocity space and returns these meshes as well as the R and Z location in [m]. 
+
+        Parameters
+        ----------
+        indexR : int
+            R index 
+        indexZ : int
+            Z index
+
+        Returns
+        -------
+        2d np array, 2d np array, 2d np array, float, float
+            f(vpar, vperp), VPAR, VPER, R [m], Z [m]
+        """
+        VPAR, VPER = np.meshgrid(self.p2f_vpar_bin_centers, self.p2f_vperp_bin_centers, indexing='ij')
+        return self.p2f_f_vpar_vper_z_r[:,:, indexZ, indexR], VPAR, VPER, self.p2f_rbin_centers[indexR], self.p2f_zbin_centers[indexZ]
+
+    def p2f_plot_distribution_function_at_RZ(
+        self,
+        R_index,
+        Z_index,
+        v_norm_over_v_max=0.015,
+        log_scale_axis_multiple=1,
+        log_clip_level=1e-6,
+        figsize=(18, 6),
+        cmap="viridis",
+        num_energy_levels=6,
+        energy_levels_linear=None,
+        energy_levels_log=None,
+        energy_color="red",
+        return_plot=False,
+        use_interpolated_rho=False,
+        rho_to_interpolate_to=None,
+        plot_trapped_passing=False,
+    ):
+        """Makes a plot of the linear and log scale p2f distribution function for species
+        versus vperp and vparallel, normalized to vnorm.
+
+        Parameters
+        ----------
+        gen_species_index : int
+            index of species s to plot
+        R_index : int
+            index of the radial coordinate in p2f Rbin centers array to plot
+        Z_index : int
+            index of the z coordinate in p2f zbin centers array to plot
+        v_norm_over_v_max : float, optional
+            x and y v/vnorm scale maximum to plot, by default 0.015
+        log_scale_axis_multiple : int, optional
+            multiplier for log axis scales so more of the distribution function is visable, by default 1
+        figsize : tuple, optional
+            figure size, by default (18, 6)
+        cmap : str, optional
+            color map, by default "viridis"
+        num_energy_levels : int, optional
+            number of energy levels to plot, by default 6
+        energy_levels_linear : list of floats, optional
+            if set, these keV energy values will be set on the linear plot, by default None
+        energy_levels_log : list of floats, optional
+            if set, these keV energy values will be set on the log plot, by default None
+        energy_color : str, optional
+            color of energy contours, by default 'red'
+        return_plot : bool, optional
+            if true, returns fig and axs objects for user manipulation, by default False
+
+        Returns
+        -------
+        matplotlib fig and ax objects
+            The fig and ax objects for user manual manipulation
+        """
+        # if use_interpolated_RZ == False:
+        #     f_s_rho, VPAR, VPERP = self.get_distribution_function_at_rho(rho_index)
+        # else:
+        #     raise ValueError('Arb. rho not implimented yet')
+        #     # f_s_rho, VPAR, VPERP = (
+        #     #     self.get_species_distribution_function_at_arbitrary_rho(
+        #     #         gen_species_index, rho=rho_to_interpolate_to
+        #     #     )
+        #     # )
+        f_s_RZ, VPAR, VPERP, R, Z = self.p2f_get_distribution_function_at_RZ(indexR=R_index, indexZ=Z_index)
+
+        # calculate energy in keV of the ions
+        mass_ion = self.species_dict[self.species]['mass']
+        E_ion = (
+            0.5
+            * mass_ion
+            * (VPAR**2 + VPERP**2)
+            / 1.6022e-19
+            / 1000
+        )
+        E_max_plot = (
+            0.5
+            * mass_ion
+            * (v_norm_over_v_max*self.vmax) ** 2
+            / 1.6022e-19
+            / 1000
+        )
+        levels_linear_E = np.linspace(0, E_max_plot, num_energy_levels).tolist()
+        levels_log_E = np.linspace(
+            0, E_max_plot * log_scale_axis_multiple**2, num_energy_levels
+        ).tolist()
+
+        fig, axs = plt.subplots(1, 2, figsize=figsize)
+
+        # linear scale subplot
+        axs[0].set_title(r'P2F f(v$_{||}$,v$_{perp}$)'+ f'\nR={R:.2f} m, Z={Z:.2f} m\n Species: {self.species}')
+
+        axs[0].set_aspect("equal")
+        axs[0].set_xlabel("$v_\parallel / v_{max}$")
+        axs[0].set_ylabel("$v_\perp / v_{max}$")
+        axs[0].set_xlim([-v_norm_over_v_max, v_norm_over_v_max])
+        axs[0].set_ylim([0, v_norm_over_v_max])
+        c1 = axs[0].contourf(VPAR/self.vmax, VPERP/self.vmax, f_s_RZ, levels=400, cmap=cmap)
+        axs[0].contour(VPAR/self.vmax, VPERP/self.vmax, f_s_RZ, levels=20, colors='black', linewidths=0.5)
+        if energy_levels_linear == None:
+            contour_lines1 = axs[0].contour(
+                VPAR/self.vmax, VPERP/self.vmax, E_ion, levels=levels_linear_E, colors=energy_color
+            )
+        else:
+            contour_lines1 = axs[0].contour(
+                VPAR/self.vmax, VPERP/self.vmax, E_ion, levels=energy_levels_linear, colors=energy_color
+            )
+
+        axs[0].clabel(
+            contour_lines1, inline=True, fontsize=8, fmt=lambda x: f"{x:.1f} [keV]"
+        )
+        fig.colorbar(
+            c1, ax=axs[0], label=r"$f_s$ [$\frac{v_{norm}^3}{(cm^3*(cm/sec)^3)}$]"
+        )
+
+        # log10 scale subplot
+
+        axs[1].set_title(
+            f"LOG10 P2F Distribution function "
+            + r'f(v$_{||}$,v$_{perp}$)'+ f'\nR={R:.2f} m, Z={Z:.2f} m\n Species: {self.species}'
+        )
+
+
+        axs[1].set_aspect("equal")
+        axs[1].set_xlabel("$v_\parallel / v_{max}$")
+        axs[1].set_ylabel("$v_\perp / v_{max}$")
+        axs[1].set_xlim(
+            [
+                -v_norm_over_v_max * log_scale_axis_multiple,
+                v_norm_over_v_max * log_scale_axis_multiple,
+            ]
+        )
+        axs[1].set_ylim([0, v_norm_over_v_max * log_scale_axis_multiple])
+        if energy_levels_log == None:
+            contour_lines2 = axs[1].contour(
+                VPAR/self.vmax, VPERP/self.vmax, E_ion, levels=levels_log_E, colors=energy_color
+            )
+        else:
+            contour_lines2 = axs[1].contour(
+                VPAR/self.vmax, VPERP/self.vmax, E_ion, levels=energy_levels_log, colors=energy_color
+            )
+
+        axs[1].clabel(
+            contour_lines2, inline=True, fontsize=8, fmt=lambda x: f"{x:.1f} [keV]"
+        )
+        c2 = axs[1].contourf(VPAR/self.vmax, VPERP/self.vmax, np.log10(np.clip(f_s_RZ, log_clip_level, None)), levels=400, cmap=cmap)
+        fig.colorbar(
+            c2,
+            ax=axs[1],
+            label=r"LOG10($f_s$) [$\frac{v_{norm}^3}{(cm^3*(cm/sec)^3)}$]",
+        )
+        axs[1].contour(VPAR/self.vmax, VPERP/self.vmax, np.log10(np.clip(f_s_RZ, log_clip_level, None)), levels=20, colors='black', linewidths=0.5)
+        # if use_interpolated_rho == False:
+        #     rho = self.rho_grid[rho_index]
+        # else:
+        #     rho=rho_to_interpolate_to
+ 
+        # if plot_trapped_passing:
+        #     B_pi = self.cal_max_B_on_flux_surface(rho, tol=1e-3)[0]
+        #     B0 = self.B_midplane_mag_interpolator(rho) 
+        #     dB = (B_pi - B0) / B0
+        #     slope = np.sqrt(1/dB)
+        #     axs[0].axline( (0,0), None, slope=slope, color='r', linestyle='--') 
+        #     axs[0].axline( (0,0), None, slope=-slope, color='r', linestyle='--')
+        #     axs[1].axline( (0,0), None, slope=slope, color='r', linestyle='--') 
+        #     axs[1].axline( (0,0), None, slope=-slope, color='r', linestyle='--')
+
+
+        if return_plot == True:
+            return fig, axs
+        plt.show()  
+
+    def p2f_convert_mcgo_particle_list_to_p2f_particle_list(self, p2f_particle_list_filename):
+        """
+        Save 1D arrays of particle data to a NetCDF file for use with p2f.
+
+        Parameters
+        ----------
+        p2f_particle_list_filename : str
+            Output path to NetCDF file (e.g., "particles.nc").
+        """
+        # read in mcgo end particle lists
+        r = self.mcgo_nc.variables['rend'][:] #[m] R-coord at t=tend
+        z = self.mcgo_nc.variables['zend'][:] #[m] Z-coord at t=tend
+        vpar= self.mcgo_nc.variables['vparend'][:] #[m/s] Vpar at t=tend
+        vperp= self.mcgo_nc.variables['vperend'][:] #[m/s] Vper at t=tend
+        weight = np.ones_like(r) # TODO for now, just assume the weights are one. Not sure how they should be set 
+
+        
+        assert all(len(arr) == len(r) for arr in [z, vperp, vpar, weight]), "All input arrays must be the same length"
+        nP = len(r)
+
+        with netCDF4.Dataset(p2f_particle_list_filename, "w", format="NETCDF4") as nc:
+            # Define the dimension
+            nc.createDimension("nP", nP)
+
+            # Create variables that p2f expects for reading 
+            r_var      = nc.createVariable("R", "f4", ("nP",))
+            z_var      = nc.createVariable("z", "f4", ("nP",))
+            vperp_var  = nc.createVariable("vPer", "f4", ("nP",))
+            vpar_var   = nc.createVariable("vPar", "f4", ("nP",))
+            weight_var = nc.createVariable("weight", "f4", ("nP",))
+
+            # Write data
+            r_var[:]      = r
+            z_var[:]      = z
+            vperp_var[:]  = vperp
+            vpar_var[:]   = vpar
+            weight_var[:] = weight
+
+            # Optional: add metadata
+            nc.title = "Particle list for p2f"
+            nc.description = "Generated from MCGO output."
+            nc.nP = nP         
+        
+        print(f'File saved to {p2f_particle_list_filename}')
+
+
